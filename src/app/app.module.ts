3bc129163422cbd1cdc6938db97873c9053c11b2
Luiz <luizbneto45@hotmail.com>
2021-11-15 11:05:28 -0300

    angular material adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoasVindasComponent } from './boas-vindas/boas-vindas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BoasVindasComponent } from './boas-vindas/boas-vindas.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
